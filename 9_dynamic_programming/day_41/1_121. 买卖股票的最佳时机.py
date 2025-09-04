8899bcd859fb26080e0a2118feaaecaa7f93a8e4
Ella_ShengYF <947393412@qq.com>
2025-09-04 08:14:08 +0800

    day38

---
+++ 9_dynamic_programming/day_41/1_121. 买卖股票的最佳时机.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = [[0] * 2 for _ in range(len(prices))]
+        dp[0][0]=-prices[0]
+        dp[0][1]=0
+
+        for i in range(1,len(prices)):
+            dp[i][0]=max(-prices[i],dp[i-1][0])
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]+prices[i])
+
+        return dp[len(prices)-1][1]
+
+prices=[7,1,5,3,6,4]
+so=Solution()
+print(so.maxProfit(prices))
